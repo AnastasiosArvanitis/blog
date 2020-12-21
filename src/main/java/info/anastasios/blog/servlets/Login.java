849ce52880af05a95a2a994fb7738de1b985a570
Anastasios Arvanitis <62649615+AnastasiosArvanitis@users.noreply.github.com>
2020-12-21 12:32:51 +0100

    login and logout servlets

--- src/main/java/info/anastasios/blog/servlets/Login.java
+++ src/main/java/info/anastasios/blog/servlets/Login.java
@@ -3,8 +3,9 @@ package info.anastasios.blog.servlets;
 import info.anastasios.blog.bll.MemberManager;
 import info.anastasios.blog.bll.PostManager;
 import info.anastasios.blog.bo.Member;
-import info.anastasios.blog.dal.MemberDaoJdbcImpl;
+import info.anastasios.blog.utlis.BlogLogger;
 
+import javax.servlet.RequestDispatcher;
 import javax.servlet.ServletException;
 import javax.servlet.http.HttpServlet;
 import javax.servlet.http.HttpServletRequest;
@@ -12,11 +13,16 @@ import javax.servlet.http.HttpServletResponse;
 import javax.servlet.http.HttpSession;
 import java.io.IOException;
 import java.sql.SQLException;
+import java.util.Objects;
+import java.util.logging.Logger;
+
 
 public class Login extends HttpServlet {
 
     private MemberManager memberManager = null;
     private PostManager postManager = null;
+
+    private Logger logger = BlogLogger.getLogger("Login");
 
     @Override
     public void init() throws ServletException {
@@ -31,23 +37,28 @@ public class Login extends HttpServlet {
     }
 
     @Override
-    protected void doPost(HttpServletRequest req, HttpServletResponse resp) throws ServletException, IOException {
-        String email = req.getParameter("email");
-        String password = req.getParameter("password");
-        Member member = new Member();
-        try {
-            member = memberManager.getMemberLogin(email, password);
-            System.out.println(member.toString());
-        } catch (SQLException throwables) {
-            throwables.printStackTrace();
-        }
+    protected void doPost(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
+        String email = request.getParameter("email");
+        String password = request.getParameter("password");
+        Member member = null;
 
-        if (member != null) {
-            HttpSession session = req.getSession();
-            session.setAttribute("member", member);
-            resp.sendRedirect("/blog/Posts");
+        if (ValidateInput.validateEmail(email) && !password.trim().equals("")) {
+            try {
+                member = memberManager.getMemberLogin(email, password);
+                if (Objects.isNull(member)) {
+                    response.sendRedirect("/blog/Error?error=userNotFound");
+                } else {
+                    HttpSession session = request.getSession();
+                    session.setAttribute("member", member);
+                    response.sendRedirect("/blog/Posts");
+                }
+
+            } catch (SQLException e) {
+                logger.severe("Error servlet Login " + e.getMessage() + "\n");
+                e.printStackTrace();
+            }
         } else {
-            resp.sendRedirect("/blog/UserNotFound.jsp");
+            response.sendRedirect("/blog/Error?error=notValidInput");
         }
     }
 }
--- src/main/java/info/anastasios/blog/servlets/Logout.java
+++ src/main/java/info/anastasios/blog/servlets/Logout.java
@@ -1,5 +1,8 @@
 package info.anastasios.blog.servlets;
 
+import info.anastasios.blog.utlis.BlogLogger;
+
+import javax.servlet.RequestDispatcher;
 import javax.servlet.ServletException;
 import javax.servlet.annotation.WebServlet;
 import javax.servlet.http.HttpServlet;
@@ -7,17 +10,27 @@ import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
 import javax.servlet.http.HttpSession;
 import java.io.IOException;
+import java.util.logging.Logger;
 
 @WebServlet(name = "Logout")
 public class Logout extends HttpServlet {
+
+    private Logger logger = BlogLogger.getLogger("Logout");
+
     protected void doPost(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
         doGet(request, response);
     }
 
     protected void doGet(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
-        HttpSession session = request.getSession();
-        session.removeAttribute("member");
-        session.invalidate();
-        response.sendRedirect("/blog/index.jsp");
+        try {
+            HttpSession session = request.getSession();
+            session.removeAttribute("member");
+            session.invalidate();
+            response.sendRedirect("/blog/");
+        } catch (Exception e) {
+            logger.severe("Error servlet Logout " + e.getMessage() + "\n");
+            e.printStackTrace();
+        }
+
     }
 }
