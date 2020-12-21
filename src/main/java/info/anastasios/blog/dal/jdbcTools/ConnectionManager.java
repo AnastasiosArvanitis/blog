3709421219bd7df97113e079a976cc3b48ed5c36
Anastasios Arvanitis <62649615+AnastasiosArvanitis@users.noreply.github.com>
2020-12-21 11:57:48 +0100

    connecting to jdbc

--- src/main/java/info/anastasios/blog/dal/jdbcTools/ConnectionManager.java
+++ src/main/java/info/anastasios/blog/dal/jdbcTools/ConnectionManager.java
@@ -1,8 +1,11 @@
 package info.anastasios.blog.dal.jdbcTools;
 
+import info.anastasios.blog.utlis.BlogLogger;
+
 import java.sql.Connection;
 import java.sql.DriverManager;
 import java.sql.SQLException;
+import java.util.logging.Logger;
 
 public class ConnectionManager {
 
@@ -11,23 +14,23 @@ public class ConnectionManager {
     private static String jdbcUserPassword = "flox123";
     private static Connection jdbcConnection;
 
+    private static Logger logger = BlogLogger.getLogger("ConnectionManager");
 
     public static Connection connect() throws SQLException {
         if (jdbcConnection == null || jdbcConnection.isClosed()) {
             try {
                 Class.forName("com.microsoft.sqlserver.jdbc.SQLServerDriver");
             } catch (ClassNotFoundException e) {
+                logger.severe("Error method listAllMembers " + e.getMessage() + "\n");
                 throw new SQLException(e);
             }
         }
         jdbcConnection = DriverManager.getConnection(jdbcUrl, jdbcUserName, jdbcUserPassword);
-        //System.out.println("connecting to the database..." + jdbcUrl);
         return jdbcConnection;
     }
 
         public static void disconnect() throws SQLException {
             if (jdbcConnection != null && !jdbcConnection.isClosed()) {
-                //System.out.println("disconnecting from the database..." + jdbcUrl);
                 jdbcConnection.close();
             }
         }
